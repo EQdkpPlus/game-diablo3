caffc3645960353cae58ed6bf2225ccf1dd294e2
Simon W <s.wallmann@kompsoft.de>
2015-12-14 22:35:19 +0100

    add class icon

--- diablo3.class.php
+++ diablo3.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('diablo3')) {
 	class diablo3 extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '1.2.0';
+		public $version				= '1.2.1';
 		protected $this_game		= 'diablo3';
 		protected $types			= array('classes');
 		protected $classes			= array();
